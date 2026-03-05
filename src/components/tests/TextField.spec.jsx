0ba8f30e3cd7d4b3b63a8d24e17fad2dd573321b
E-ppo <93433413+E-ppo@users.noreply.github.com>
2026-03-05 22:48:19 +0900

    test: TextField onChange, onEnter, onFocus 테스트 작성 및 describe/beforeEach 적용

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,6 +4,12 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
+// my-class라는 class가 항상 적용된 컴포넌트를 렌더링
+
+// beforeEach(async () => {
+//   await render(<TextField className="my-class" />);
+// });
+
 it('className prop으로 설정한 css class가 적용된다.', async () => {
   // 1) Arrange - 테스트를 위한 환경 만들기  -> className을 지닌 컴포넌트 렌더링
   // render API를 호출 -> 테스트 환경의 jsDOM에 리엑트 컴포넌트가 렌더링 된 DOM 구조가 반영
@@ -31,34 +37,11 @@ it('className prop으로 설정한 css class가 적용된다.', async () => {
 // it -> test 함수의 alias
 // it -> 'should~~~' 또는 test -> 'if~~~' 적용
 // describe -> it 또는 test 함수들을 그룹핑하여 테스트를 진행
-it('기본 placeholder "텍스트를 입력 해주세요."가 노출된다.', async () => {
+describe('placeholder', () => {
   // 기대결과 === 실제 결과 --> 성공
   // 기대결과 !== 실제 결과 --> 실패
 
-  // Arrange
-  await render(<TextField />);
-
-  // Act 생략
-
-  // Assert
-  // 단언(assertion) -> 테스트가 통과하기 위한 조건 -> 검증 실행
-  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
-  expect(textInput).toBeInTheDocument();
-});
-
-it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
-  // Arrange
-  await render(<TextField placeholder={'플레이스 홀더가 변경'} />);
-
-  // Act 생략
-
-  // Assert
-  const textInput = screen.getByPlaceholderText('플레이스 홀더가 변경');
-  expect(textInput).toBeInTheDocument();
-});
-
-describe('placeholder', () => {
-  it('기본 placeholder "텍스트를 입력 해주세요."가 노출된다.', async () => {
+  it('[describe placeholder] 기본 placeholder "텍스트를 입력 해주세요."가 노출된다.', async () => {
     // Arrange
     await render(<TextField />);
 
@@ -70,7 +53,7 @@ describe('placeholder', () => {
     expect(textInput).toBeInTheDocument();
   });
 
-  it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
+  it('[describe placeholder]placeholder prop에 따라 placeholder가 변경된다.', async () => {
     // Arrange
     await render(<TextField placeholder={'플레이스 홀더가 변경'} />);
 
@@ -80,4 +63,81 @@ describe('placeholder', () => {
     const textInput = screen.getByPlaceholderText('플레이스 홀더가 변경');
     expect(textInput).toBeInTheDocument();
   });
+});
+
+it('텍스트를 입력하면 onChange prop으로 등록한 함수가 호출된다.', async () => {
+  // Arrange
+  const spy = vi.fn(); // spy 함수 -> 특정 함수의 호출 여부, 호출 횟수, 전달된 인자 등을 추적할 수 있는 도구
+  const { user } = await render(<TextField onChange={spy} />);
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  // Act
+  await user.type(textInput, 'test');
+
+  // Assert
+  expect(spy).toHaveBeenCalledWith('test');
+});
+
+it('엔터키를 입력하면 onEnter prop으로 등록한 함수가 호출된다.', async () => {
+  // Arrange
+  const spy = vi.fn();
+  const { user } = await render(<TextField onEnter={spy} />);
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  // Act
+  await user.type(textInput, 'test{enter}');
+
+  // Assert
+  expect(spy).toHaveBeenCalledWith('test');
+});
+
+describe('focus', () => {
+  // 포커스 활성화 조건
+  // 1) 탭 키로 인풋 요소에 포커스
+  // 2) 마우스로 인풋 요소 클릭
+  // 3) textInput.focus() 메서드로 포커스 활성화
+  // onFocus는 별도 인자가 필요없는 함수임.
+  // toHaveBeenCalled : spy 함수가 호출되었는지 검증하는 역할을 수행함
+
+  // Arrange 분리
+  let spy;
+  let user;
+  let textInput;
+
+  beforeEach(async () => {
+    spy = vi.fn();
+    ({ user } = await render(<TextField onFocus={spy} />));
+    textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+  });
+
+  it('탭키로 포커스되면 onFocus prop으로 등록한 함수가 호출된다.', async () => {
+    await user.tab();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('클릭으로 포커스되면 onFocus prop으로 등록한 함수가 호출된다.', async () => {
+    await user.click(textInput);
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('textInput.focus() 메서드로 포커스되면 onFocus prop으로 등록한 함수가 호출된다.', async () => {
+    textInput.focus();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('포커스가 활성화되면 border 스타일이 변경된다.', async () => {
+    await user.click(textInput);
+
+    expect(textInput).toHaveStyle({
+      borderWidth: '2px',
+      borderColor: 'rgb(25, 118, 210)',
+    });
+  });
+
+  // afterEach에 expect를 넣으면 안되는 이유
+  // afterEach는 정리(cleanup) 용도이고, 검증(assert)*은 it 안에 있어야
+  // 어떤 테스트가 왜 실패했는지 명확하게 알 수 있기 때문에 넣으면 안된다.
 });
--- src/utils/test/render.jsx
+++ src/utils/test/render.jsx
@@ -1,8 +1,12 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+/**
+ * userEvent : 사용자 이벤트 시뮬레이터 -> 클릭, 입력, 포커스 등등의 사용자 행동을 시뮬레이션하는 도구
+ */
+
 export default async component => {
-  const user = userEvent.setup();
+  const user = userEvent.setup(); // 사용자 이벤트 시뮬레이터 설정 -> 비동기 이벤트 처리 지원
 
   return {
     user,
